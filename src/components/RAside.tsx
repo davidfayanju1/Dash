149da11155ae0ebe06238234dce03d1fd93e737a
davidfayanju1 <davidfayanju01@gmail.com>
2025-01-31 16:33:52 +0100

    Initial commit

---
+++ src/components/RAside.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RAside = () => {
+  return (
+    <div className="w-full h-full bg-white border-r-2 border-gray-200">
+      <div className="logo-container"></div>
+    </div>
+  );
+};
+
+export default RAside;
---
+++ src/layout/DashLayout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DashLayout = () => {
+  return (
+    <div className="page-container">
+      <aside className="aside-component"></aside>
+      <main className="main-component"></main>
+      <aside className="final-aside"></aside>
+    </div>
+  );
+};
+
+export default DashLayout;
