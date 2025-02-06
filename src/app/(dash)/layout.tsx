c21ea1ecfb16345fb43bd20fba76f9edfcdc8ca8
davidfayanju1 <davidfayanju01@gmail.com>
2025-02-06 15:35:55 +0100

    Finishing up the user mgt page

--- src/app/(dash)/layout.tsx
+++ src/app/(dash)/layout.tsx
@@ -4,6 +4,8 @@ import LAside from "@/components/LAside";
 import RAside from "@/components/RAside";
 import React, { useState } from "react";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import { useTheme } from "@/ThemeContext";
 
 type DashLayoutProp = {
   children: React.ReactNode;
@@ -12,6 +14,8 @@ type DashLayoutProp = {
 function Dashboard({ children }: DashLayoutProp) {
   const [toggle, setToggle] = useState<boolean>(false);
   const [toggleRight, setToggleRight] = useState<boolean>(false);
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <>
       <div
@@ -45,6 +49,135 @@ function Dashboard({ children }: DashLayoutProp) {
 
         <div className="flex-container ml-0 lg:ml-[16rem] min-h-screen w-full flex items-start justify-start md:flex-row flex-col">
           <div className="w-full lg:w-[calc(100%-16rem)] xl:w-[calc(100%-16rem)] min-h-[100vh]">
+            <nav
+              className={`nav-container md:px-[2rem] px-[1rem] py-[1.4rem] border-b-[1px] flex items-center justify-between ${
+                theme === "dark" ? "border-gray-600" : "border-gray-200"
+              }`}
+            >
+              <div className="first-item-container flex items-center gap-3  h-full">
+                <button
+                  onClick={() => setToggle(!toggle)}
+                  className="outline-none"
+                >
+                  <Image
+                    src={theme === "dark" ? "/booklight.svg" : "/book.svg"}
+                    alt="svg-component"
+                    width={30}
+                    height={30}
+                  />
+                </button>
+                <Image
+                  src={theme === "dark" ? "/starlight.svg" : "star.svg"}
+                  alt="svg-component"
+                  width={30}
+                  height={30}
+                />
+
+                <small
+                  className={`md:block hidden ${
+                    theme === "dark" ? "text-white/40" : "text-black/40"
+                  }`}
+                >
+                  Dashboard /
+                  <span
+                    className={`text-black ${
+                      theme === "dark" ? "text-gray-100" : "text-black/40"
+                    }`}
+                  >
+                    Default
+                  </span>
+                </small>
+              </div>
+              <div className="second-item-container flex items-center justify-center gap-3">
+                <div
+                  className={`search-input mr-[1rem] px-[.8rem]  ${
+                    theme === "light" ? "bg-gray-100" : "bg-white/10"
+                  } h-[2.5rem] rounded-[9px] md:flex hidden items-center w-[70%]`}
+                >
+                  <div className="image-container">
+                    <Image
+                      src={
+                        theme === "light" ? "/search.svg" : "/darksearch.svg"
+                      }
+                      alt="search-icon"
+                      width={16}
+                      height={16}
+                    />
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="Search"
+                    className={`px-2 ${
+                      theme === "dark"
+                        ? " placeholder:text-white/40"
+                        : "placeholder:text-gray-300"
+                    } bg-transparent border-none outline-none text-[16px]`}
+                  />
+                  {/* <div className="image-cont">
+                          <Image
+                            src={theme === "light" ? "/search2.svg" : "/search2dark.svg"}
+                            alt="search-2-icon"
+                            width={20}
+                            height={20}
+                          />
+                        </div> */}
+                </div>
+
+                <div className="toggle-section flex items-center justify-center gap-4">
+                  <div onClick={toggleTheme}>
+                    {theme === "dark" ? (
+                      <Image
+                        src={"/darkbutton.svg"}
+                        alt="toggle-button-icon"
+                        width={35}
+                        height={35}
+                      />
+                    ) : (
+                      <Image
+                        src={"/lightbutton.svg"}
+                        alt="toggle-button-icon"
+                        width={35}
+                        height={35}
+                      />
+                    )}
+                  </div>
+
+                  <div className="timer-container md:block hidden">
+                    {theme === "dark" ? (
+                      <Image
+                        src={"/timerlight.svg"}
+                        alt="timer-icon"
+                        width={35}
+                        height={35}
+                      />
+                    ) : (
+                      <Image
+                        src={"/timer.svg"}
+                        alt="timer-icon"
+                        width={35}
+                        height={35}
+                      />
+                    )}
+                  </div>
+
+                  <div
+                    onClick={() => setToggleRight(true)}
+                    className="notifications-container cursor-pointer"
+                  >
+                    <Image
+                      src={
+                        theme === "dark"
+                          ? "/notificationslight.svg"
+                          : "/notifications.svg"
+                      }
+                      alt="notification toggle-icon"
+                      width={30}
+                      height={30}
+                    />
+                  </div>
+                </div>
+              </div>
+            </nav>
             {children}
           </div>
           <div className="fixed overflow-y-scroll top-0 right-0 custom:block hidden w-[16rem] h-screen no-scrollbar">
--- src/app/(dash)/usermgt/page.tsx
+++ src/app/(dash)/usermgt/page.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 const UserMgt = () => {
-  return <div>UserMgt</div>;
+  return (
+    <div className="page-container md:px-[2rem] px-[1rem]">
+      <div className="title-container mt-[2rem] flex items-center justify-between">
+        <h1 className="title-text md:text-[1.3rem] text-[1rem] font-semibold">
+          User Management
+        </h1>
+
+        <div className="toggle-container">
+          <Image src={"/toggle.svg"} alt="toggle-icon" width={34} height={34} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserMgt;
